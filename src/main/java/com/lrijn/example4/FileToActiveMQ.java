0979f1f35caca4b3be89b26c37025e2e7ce173ed
ILIONX\lvanrijn <lvanrijn@ilionx.com>
2025-05-19 12:05:17 +0200

    remove not working FileToActiveMQ class

--- src/main/java/com/lrijn/example4/FileToActiveMQ.java
+++ src/main/java/com/lrijn/example4/FileToActiveMQ.java
@@ -1,7 +1,5 @@
 package com.lrijn.example4;
 
-import java.util.Set;
-
 import javax.jms.ConnectionFactory;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
@@ -11,31 +9,24 @@ import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class FileToActiveMQ {
-	
-	public static void main(String[] args) throws Exception {
-		CamelContext context = new DefaultCamelContext();
-		
-		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-		context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
-		
-		context.addRoutes(new RouteBuilder() {
-			
-			@Override
-			public void configure() throws Exception {
-				
-				from("file:input_box?noop=true")
-				.to("activemq:queue:my_queue");
-				
-			}
-		});
-		
-		while(true)
-			context.start();
-		
-	}
-	
-	
-	
-			
 
+    public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+
+        context.addComponent("activemq", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:input_box?noop=true")
+                .to("activemq:queue:my_queue");
+            }
+        });
+        
+        context.start();
+
+        Thread.sleep(Long.MAX_VALUE);
+    }
 }
