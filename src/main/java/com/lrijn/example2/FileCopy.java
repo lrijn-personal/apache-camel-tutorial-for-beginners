baa584d38c0b7338fd399a034354e6f99a1ef86c
ILIONX\lvanrijn <lvanrijn@ilionx.com>
2025-05-19 11:53:16 +0200

    add examples

---
+++ src/main/java/com/lrijn/example2/FileCopy.java
@@ -0,0 +1,18 @@
+
+package com.lrijn.example2;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+
+public class FileCopy {
+
+    public static void main(String[] args) throws Exception {
+
+        CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new FileCopyRoute());
+        while(true)
+            context.start();
+    }
+}
---
+++ src/main/java/com/lrijn/example6/MyService.java
@@ -0,0 +1,8 @@
+package com.lrijn.example6;
+
+public class MyService {
+	public void doSomething(String message) {
+		System.out.println(message);
+	}
+
+}
